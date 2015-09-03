e6fa16efd8d6651db4a34e63b2b79fb45291c5c9
bjarnef <bjarne_fyrstenborg@hotmail.com>
2015-09-04 00:59:48 +0200

    Add export and import feature
    Added option to export and import existing palettes.

--- ColorPalettes/ColorPalettes/ColorPalettes/controllers/ColorPalettesSettings.Controller.js
+++ ColorPalettes/ColorPalettes/ColorPalettes/controllers/ColorPalettesSettings.Controller.js
@@ -1,14 +1,88 @@
-﻿angular.module("umbraco").controller("Our.Umbraco.ColorPalettes.Settings.Controller", function ($scope, $timeout, notificationsService, dialogService, angularHelper, localizationService) {
+﻿var app = angular.module("umbraco");
 
+app.requires.push('ngFileUpload');
+
+app.controller("Our.Umbraco.ColorPalettes.Settings.Controller", function ($scope, $timeout, notificationsService, dialogService, angularHelper, localizationService, Upload) {
+    
     var editDialog = null;
+    var alreadyDirty = false;
 
     //$scope.model.value = "";
     //console.log($scope.model.value);
-
+    
+    //init the palettes
     $scope.palettes = angular.fromJson($scope.model.value) || [];
 
-    //init the model
-    $scope.model.value = $scope.model.value || $scope.palettes;
+    // Blob support
+    $scope.isBlobSupported = function () {
+        var isFileSaverSupported;
+
+        try {
+            isFileSaverSupported = !!new Blob;
+        }
+        catch (e) {
+            isFileSaverSupported = false;
+        }
+
+        return isFileSaverSupported;
+    };
+
+    /* used for choosing palettes to export */
+    var exportConfig = {};
+    $scope.exportConfig = exportConfig;
+
+    exportConfig.allItemsSelected = false;
+
+    // Fired when an entity in the table is checked
+    $scope.selectPalette = function () {
+        // If any entity is not checked, then uncheck the "allItemsSelected" checkbox
+        for (var i = 0; i < $scope.palettes.length; i++) {
+            if (!$scope.palettes[i].isChecked) {
+                exportConfig.allItemsSelected = false;
+                return;
+            }
+        }
+
+        // ... otherwise ensure that the "allItemsSelected" checkbox is checked
+        exportConfig.allItemsSelected = true;
+    };
+
+    $scope.selectAll = function () {
+        // Loop through all the entities and set their isChecked property
+        for (var i = 0; i < $scope.palettes.length; i++) {
+            $scope.palettes[i].isChecked = exportConfig.allItemsSelected;
+        }
+    };
+
+    $scope.anySelected = function () {
+        for (var i = 0; i < $scope.palettes.length; i++) {
+            if ($scope.palettes[i].isChecked == true) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    $scope.countSelected = function () {
+        var count = 0;
+        for (var i = 0; i < $scope.palettes.length; i++) {
+            if ($scope.palettes[i].isChecked == true) {
+                count++;
+            }
+        }
+        return count;
+    };
+
+    /* --------------------------------- */
+
+    function isJson(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    };
 
     $scope.addPalette = function () {
 
@@ -78,8 +152,41 @@
         }
     }
 
+    $scope.getPalettesToExport = function () {
+        var array = [];
+        for (var i = 0; i < $scope.palettes.length; i++) {
+            if ($scope.palettes[i].isChecked == true) {
+                var _palette = angular.copy($scope.palettes[i]);
+
+                //remove properties from output
+                delete _palette.isChecked;
+                delete _palette.selection;
+
+                array.push(_palette);
+            }
+        }
+        
+        var json = angular.toJson(array, true);
+        return json;
+    }
+
+    $scope.exportPalette = function (palette) {
+        var _palette = angular.copy(palette);
+
+        //remove property from output
+        delete _palette.isChecked;
+
+        var array = [];
+        array.push(_palette);
+        var json = angular.toJson(array, true);
+        return json;
+    }
+
     //sync things up on save
     $scope.$on("formSubmitting", function (ev, args) {
+        for (var i = 0; i < $scope.palettes.length; i++) {
+            delete $scope.palettes[i].isChecked;
+        }
         $scope.model.value = angular.toJson($scope.palettes);
     });
 
@@ -101,5 +208,83 @@
 
         }
     };
+
+    $scope.uploadFiles = function (file) {
+        $scope.f = file;
+        //console.log($scope.f);
+        
+        if (file && file.$error == 'pattern') {
+            notificationsService.error("Error", "Invalid file type - only .txt or .json allowed.");
+            return;
+        }
+
+        if (file && !file.$error) {
+            var reader = new FileReader();
+
+            reader.readAsText(file);
+
+            reader.onloadend = function () {
+                var result = reader.result;
+
+                if (!isJson(result)) {
+                    notificationsService.error("Error", "The file does not contain valid JSON.");
+                    return;
+                }
+
+                var importedPalettes = angular.fromJson(JSON.parse(result));
+                //console.log("importedPalettes", importedPalettes);
+
+                var importCount = 0,
+                    importErrorCount = 0;
+
+                for (var i = 0; i < importedPalettes.length; i++) {
+                    //console.log(importedPalettes[i]);
+                    
+                    // check if palette alias already exists
+                    var found = false;
+                    for (var j = 0; j < $scope.palettes.length; j++) {
+                        if ($scope.palettes[j].alias == importedPalettes[i].alias) {
+                            found = true;
+                            importErrorCount++;
+                            break;
+                        }
+                    }
+
+                    // add palette is not existing alias was found
+                    if (!found) {
+                        $scope.palettes.push(importedPalettes[i]);
+                        importCount++;
+                    }
+                }
+
+                $scope.model.value = angular.toJson($scope.palettes);
+
+                if (importErrorCount > 0) {
+                    var errorCountText = importErrorCount + " " + (importErrorCount == 1 ? "palette" : "palettes");
+                    notificationsService.error("Error", errorCountText + " were not imported, because palette alias(es) were found!");
+                }
+
+                if (importCount > 0) {
+                    var importCountText = importCount + " new " + (importCount == 1 ? "palette" : "palettes");
+                    notificationsService.success("Success", importCountText + " has been imported. Remember to save changes!");
+                }
+            }
+        }
+    };
+
+    // test if model.value has changed
+    $scope.$watch('model.value', function (newval, oldval) {
+        //console.log(newval, oldval);
+
+        if (newval !== oldval) {
+            $timeout(function () {
+                if (!alreadyDirty) {
+                    var currForm = angularHelper.getCurrentForm($scope);
+                    currForm.$setDirty();
+                    alreadyDirty = true;
+                }
+            }, 0);
+        }
+    });
 
 });
---
+++ ColorPalettes/ColorPalettes/ColorPalettes/directives/PaletteDownload.js
@@ -0,0 +1,52 @@
+﻿var app = angular.module("umbraco");
+app.requires.push('fileSaver');
+
+app.directive('paletteDownload', function ($compile, SaveAs) {
+        return {
+            restrict: 'AE',
+            scope: {
+                data: '=',
+                text: '@',
+                isDisabled: '='
+            },
+            link: function (scope, elm, attrs) {
+
+                var date = new Date(),
+                    month = (date.getMonth() + 1) > 9 ? (date.getMonth() + 1) : "0" + (date.getMonth() + 1),
+                    day = (date.getDate() + 1) > 9 ? (date.getDate() + 1) : "0" + (date.getDate() + 1),
+                    hours = (date.getHours()) > 9 ? (date.getHours()) : "0" + (date.getHours()),
+                    minutes = (date.getMinutes()) > 9 ? (date.getMinutes()) : "0" + (date.getMinutes()),
+                    seconds = (date.getSeconds()) > 9 ? (date.getSeconds()) : "0" + (date.getSeconds()),
+                    dateFormatted = month + "-" + day + "-" + date.getFullYear() + "-" + hours + ":" + minutes + ":" + seconds;
+
+                var buttonText = scope.text ? scope.text : "Download",
+                    isDisabled = (angular.isDefined(scope.isDisabled)) ? 'ng-disabled="isDisabled"' : null;
+
+                scope.onHandleClick = function () {
+
+                    var json = scope.data,
+                        blob = new Blob([json], { type: "application/json;charset=utf-8" }),
+                        filename = "palettes";
+
+                    var options = { type: "application/json;charset=utf-8" };
+                    SaveAs.download(blob, filename + '-' + dateFormatted + '.json', options);
+                };
+
+                elm.append($compile(
+                        '<button type="button" class="btn btn-small" ' + isDisabled + ' ng-click="onHandleClick()">' +
+                        '<i class="icon-download-alt"></i> ' + buttonText +
+                        '</button>'
+                )(scope));
+
+                //scope.$watch('isDisabled', function (newVal) {
+                //    console.log("scope.isDisabled", newVal);
+
+                //    elm.children()[0].attr('disabled', newVal);
+                //});
+
+                //scope.$parent.$watch(attrs.ngDisabled, function (newVal) {
+                //    console.log("attrs.ngDisabled", newVal);
+                //});
+            }
+        };
+    });
